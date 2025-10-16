ffb6621ce5bd0b277ea8489d5fc727a88957685a
SakethMaddi <158171895+SakethMaddi@users.noreply.github.com>
2025-10-16 22:35:00 +0530

    Delete adidas directory

--- adidas/store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('shoes/',view=views.shoes),
-    path('men/',view=views.men),
-    path('women/',view=views.women),
-    path('kids/',view=views.kids)
-]
